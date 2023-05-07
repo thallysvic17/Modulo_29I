9ec73aa3448913b1970158d1b1eea60f9871f815
thallysvic17 <thallysvic17@gmail.com>
2023-05-07 16:50:33 -0300

    inicial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Teste from './teste';
+import GlobalStyle from './styles';
+
+function App() {
+  return (
+    <>
+    
+    <GlobalStyle/>
+    <Teste/>
+
+    </>
+
+  )
+}
+
+export default App;
+
